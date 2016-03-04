0899b5256dca182d2db706c7eedcfc1665cda614
1807257646 <scsys@xiejiangbo.local>
2016-03-04 09:57:41 +0800

    Initial Commit

---
+++ GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub
+//
+//  Created by scsys on 16/3/4.
+//  Copyright © 2016年 Breen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHub
+//
+//  Created by scsys on 16/3/4.
+//  Copyright © 2016年 Breen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
